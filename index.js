1f3e990679dd92baa80dc5aa1b96838981daafed
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2019-01-17 17:31:24 -0600

    Se agrego primer comopnente llamado Media

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hola mundo!!!!')
+import React from 'react'
+import {render} from 'react-dom'
+import Media from './src/playlist/components/media'
+
+const app = document.getElementById('app')
+
+render(<Media/>, app)
