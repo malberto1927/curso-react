44eddf384a11846510299ec2ce4ad5bbe47a5d5a
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2019-01-17 18:42:35 -0600

    Se agregaron props y events al componente

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import Media from './src/playlist/components/media'
 
 const app = document.getElementById('app')
 
-render(<Media/>, app)
+render(<Media title="¿Qué es responsive design?" author="Mario Alberto" image="./images/covers/responsive.jpg"/>, app)
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,18 +1,30 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import './media.css'
 
 class Media extends Component {
+
+    handlerClick = (event) => {
+        console.log(this.props.title)
+    }
+
     render() {
         return (
-            <div className="Media">
+            <div className="Media" onClick={this.handlerClick}>
                 <div className="Media-cover">
-                    <img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
-                    <h3 className="Media-title">¿Por qué aprender React?</h3>
-                    <p className="Media-author">Mario Alberto</p>
+                    <img className="Media-image" src={this.props.image} alt="" width={260} height={160}/>
+                    <h3 className="Media-title">{this.props.title}</h3>
+                    <p className="Media-author">{this.props.author}</p>
                 </div>
             </div>
         )
     }
+}
+
+Media.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string
 }
 
 export default Media
