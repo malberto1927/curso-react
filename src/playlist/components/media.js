de501634061cfb2606c98a2c7eeb177a7bf4296f
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2019-01-17 18:10:33 -0600

    Se agregarn estilos al componente Media

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
+import './media.css'
 
 class Media extends Component {
     render() {
         return (
-            <div>
-                <div>
-                    <img src="" alt="" width={260} height={160}/>
-                    <h3>¿Por qué aprender React?</h3>
-                    <p>Mario Alberto</p>
+            <div className="Media">
+                <div className="Media-cover">
+                    <img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
+                    <h3 className="Media-title">¿Por qué aprender React?</h3>
+                    <p className="Media-author">Mario Alberto</p>
                 </div>
             </div>
         )
